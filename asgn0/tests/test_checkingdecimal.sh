b32720989c072b225f2aa20838e11a42e49c15f3
demajumd <demajumd@ucsc.edu>
2024-01-20 07:30:18 +0000

    commit

---
+++ asgn0/tests/test_checkingdecimal.sh
@@ -0,0 +1,30 @@
+# Creates the example output
+echo BAD INPUT > expected.txt
+
+# Runs the program
+./calc 3 > output.txt 
+
+# Ensures exit code is Zero
+if [ $? -eq 0 ]; then
+    echo "do decimals work" $?
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Ensures differences *are* found
+diff expected.txt output.txt
+if [ $? -eq 0 ]; then
+    echo "Somehow, the decimals are adding?!"
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Test does not accpet decimals: PASS"
+
+# Cleans up files created
+rm output.txt
+rm expected.txt
+exit 0
