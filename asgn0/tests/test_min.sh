afa12bc4c2942bb91f5d873af6b8fbbab0f8df2c
demajumd <demajumd@ucsc.edu>
2024-01-21 02:41:24 +0000

    yo

---
+++ asgn0/tests/test_min.sh
@@ -0,0 +1,30 @@
+# Creates the example output
+echo -1024 > expected.txt
+
+# Runs the program
+./calc -512 -512 > output.txt 
+
+# Ensures exit code is Zero
+if [ $? -ne 0 ]; then
+    echo "invalid exit code" $?
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Ensures differences *are* found
+diff output.txt expected.txt
+if [ $? -ne 0 ]; then
+    echo "Somehow, -511-511 doesn't equal -1022!!"
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Test does -511-511 = -1022: PASS"
+
+# Cleans up files created
+rm output.txt
+rm expected.txt
+exit 0
---
+++ asgn0/tests/test_negativeint.sh
@@ -0,0 +1,30 @@
+# Creates the example output
+echo -2 > expected.txt
+
+# Runs the program
+./calc -6 -4 > output.txt 
+
+# Ensures exit code is Zero
+if [ $? -ne 0 ]; then
+    echo "did not work" $?
+    rm expected.txt
+    rm output.txt
+    exit 1
+fi
+
+# Ensures differences *are* found
+diff expected.txt output.txt
+if [ $? -ne 0 ]; then
+    echo "Somehow, the output does not say -6 +4 = -2!!"
+    rm expected.txt
+    rm output.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Test does -6 + 4 = -2: PASS"
+
+# Cleans up files created
+rm output.txt
+rm expected.txt
+exit 0
