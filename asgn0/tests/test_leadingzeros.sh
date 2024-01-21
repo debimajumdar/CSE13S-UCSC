99f6f65d0a43f86a79ab6a5bb382650530644ca5
demajumd <demajumd@ucsc.edu>
2024-01-21 03:53:24 +0000

    please work

--- asgn0/tests/test_leadingzeros.sh
+++ asgn0/tests/test_leadingzeros.sh
@@ -1,5 +1,5 @@
 # Creates the example output
-echo 15 > output.txt
+echo 17 > output.txt
 
 # Runs the program
 ./calc 008 009 > expected.txt 
@@ -15,14 +15,14 @@ fi
 # Ensures differences *are* found
 diff output.txt expected.txt
 if [ $? -ne 0 ]; then
-    echo "Somehow, the output isn't 15!"
+    echo "Somehow, the output isn't 17!"
     rm output.txt
     rm expected.txt
     exit 1
 fi
 
 # Prints a message on success
-echo "Test finds that 008+009 = 15: PASS"
+echo "Test finds that 008+009 = 17: PASS"
 
 # Cleans up files created
 rm output.txt
