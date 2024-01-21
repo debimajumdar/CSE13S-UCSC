0461a3485295756b64b56d5d03ec90f3371193f6
demajumd <demajumd@ucsc.edu>
2024-01-21 03:00:07 +0000

    idk

---
+++ asgn0/tests/test_capslock.sh
@@ -0,0 +1,30 @@
+# Creates the example output
+echo BAD INPUT > output.txt
+
+# Runs the program
+./calc 3 A> expected.txt 
+
+# Ensures exit code is Zero
+if [ $? -eq 0 ]; then
+    echo "failed badinput" $?
+    rm expected.txt
+    rm output.txt
+    exit 1
+fi
+
+# Ensures differences *are* found
+diff expected.txt output.txt
+if [ $? -ne 0 ]; then
+    echo "Somehow, a caps lock character is accepted in the second slot!"
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Test does not accept caps lock characters in second slot : FAIL"
+
+# Cleans up files created
+rm output.txt
+rm expected.txt
+exit 0
