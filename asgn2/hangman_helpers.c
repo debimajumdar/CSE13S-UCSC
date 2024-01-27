864457b7b502271c0446354006c0f811e282a060
demajumd <demajumd@ucsc.edu>
2024-01-27 02:20:57 +0000

    adding files

---
+++ asgn2/hangman_helpers.c
@@ -0,0 +1,13 @@
+#include "hangman_helpers.h"
+
+bool is_lowercase_letter(char c) {
+}
+
+bool validate_secret(const char *secret) {
+}
+
+bool string_contains_character(const char *s, char c) {
+}
+
+char read_letter(void) {
+}
