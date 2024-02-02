12007adb2caf38f425bfbb7063a6c93dca81e363
demajumd <demajumd@ucsc.edu>
2024-02-02 07:10:04 +0000

    hey

--- asgn2/hangman.c
+++ asgn2/hangman.c
@@ -1,17 +1,9 @@
-// hangman.c
-
 #include "hangman_helpers.h"
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Declaration for the missing function
-bool has_guessed_all(const char *secret, const char *guessed_letters);
-void initialize_game(const char *secret, char *phrase);
-bool is_game_over(const char *secret, const char *phrase, const char *eliminated, int incorrect_attempts);
-void print_game_state(const char *arts[], int gallows_state, const char *phrase, const char *eliminated);
-void update_phrase(const char *secret, char *phrase, char guess);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -21,26 +13,24 @@ int main(int argc, char *argv[]) {
             argv[0]);
         return 1;
     }
-
-    const char *user_secret = argv[1];
-    if (!validate_secret(user_secret)) {
+    const char *user_secret = argv[1]; // The secret word is the second argument
+    if (!(validate_secret(user_secret))) {
         return 1;
     }
-
     char secret[MAX_LENGTH + 1] = { 0 };
     char phrase[MAX_LENGTH + 1] = { 0 };
-    char eliminated[26] = { 0 };
+    char eliminated[26] = { 0 }; // An array to store eliminated letters
     int eliminated_count = 0;
     int incorrect_attempts = 0;
+    bool game_over = false;
     int gallows_state = 0;
-
     strncpy(secret, user_secret, MAX_LENGTH);
+
     initialize_game(secret, phrase);
 
-    while (!is_game_over(secret, phrase, eliminated, incorrect_attempts)) {
+    while (!game_over) {
         char guess;
         print_game_state(arts, gallows_state, phrase, eliminated);
-
         while (1) {
             printf("Guess a letter: ");
             guess = read_letter();
@@ -51,16 +41,16 @@ int main(int argc, char *argv[]) {
                        || string_contains_character(phrase, guess)) {
                 continue;
             } else {
-                break;
+                break; // Valid letter entered
             }
         }
-
         if (string_contains_character(secret, guess)) {
             update_phrase(secret, phrase, guess);
-            if (has_guessed_all(secret, phrase)) {
+            if (strcmp(user_secret, phrase) == 0) {
                 print_game_state(arts, gallows_state, phrase, eliminated);
+
                 printf("You win! The secret phrase was: %s\n", secret);
-                break;
+                game_over = true;
             }
         } else {
             eliminated[eliminated_count++] = guess;
@@ -69,11 +59,12 @@ int main(int argc, char *argv[]) {
 
             if (incorrect_attempts == LOSING_MISTAKE) {
                 print_game_state(arts, gallows_state, phrase, eliminated);
+
                 printf("You lose! The secret phrase was: %s\n", secret);
-                break;
+                game_over = true;
             }
         }
     }
-
     return 0;
 }
+
