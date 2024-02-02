c3bee0cebea927ecc8b70d3bf89bdb61c6b48c98
demajumd <demajumd@ucsc.edu>
2024-02-02 00:03:34 +0000

    hello

--- asgn2/hangman.c
+++ asgn2/hangman.c
@@ -5,63 +5,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-void play_game(const char *secret) {
-    char guessed_letters[27] = { 0 }; // array for ALL guessed letters
-    char incorrect_chars[27] = { 0 }; // array for ONLY WRONG character inputs
-    int mistakes = 0; // increments
-
-    while (!is_game_over(secret, guessed_letters, mistakes)) {
-        printf(CLEAR_SCREEN);
-
-        printf("%s", arts[mistakes]);
-        phrase(secret, guessed_letters);
-        show_incorrect_guesses(incorrect_chars);
-        printf("\n");
-
-        char guess = read_letter();
-        while (string_contains_character(guessed_letters, guess) || !is_lowercase_letter(guess)) {
-            guess = read_letter(); // prompt user until a valid guess is received
-        }
-
-        strncat(guessed_letters, &guess, 1); // append guess to guessed letters
-
-        if (!string_contains_character(secret, guess)) {
-            if (is_lowercase_letter(guess)) {
-                strncat(incorrect_chars, &guess, 1);
-            }
-            mistakes++;
-        }
-    }
-
-    if (mistakes == LOSING_MISTAKE) {
-        printf(CLEAR_SCREEN);
-        printf("%s", arts[mistakes]);
-        phrase(secret, guessed_letters);
-        show_incorrect_guesses(incorrect_chars);
-        printf("\n");
-        printf("You lose! The secret phrase was: %s\n", secret);
-    } else if (has_guessed_all(secret, guessed_letters)) {
-        printf(CLEAR_SCREEN);
-        printf("%s", arts[mistakes]);
-        phrase(secret, guessed_letters);
-        show_incorrect_guesses(incorrect_chars);
-        printf("\n");
-        printf("You win! The secret phrase was: %s\n", secret);
-    }
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr,
-            "wrong number of arguments\nusage: ./hangman <secret word or phrase>\nif the secret "
-            "is multiple words, you must quote it\n");
-        exit(1);
+            "wrong number of arguments\nusage: %s <secret word or phrase>\nif the secret is "
+            "multiple words, you must quote it\n",
+            argv[0]);
+        return 1;
     }
-
-    if (!validate_secret(argv[1])) {
-        exit(1);
+    const char *user_secret = argv[1]; // The secret word is the second argument
+    if (!(validate_secret(user_secret))) {
+        return 1;
     }
+    char secret[MAX_LENGTH + 1] = { 0 };
+    char phrase[MAX_LENGTH + 1] = { 0 };
+    char eliminated[26] = { 0 }; // An array to store eliminated letters
+    int eliminated_count = 0;
+    int incorrect_attempts = 0;
+    bool game_over = false;
+    int gallows_state = 0;
+    strncpy(secret, user_secret, MAX_LENGTH);
 
-    play_game(argv[1]);
+    initialize_game(secret, phrase);
+
+    while (!game_over) {
+        char guess;
+        print_game_state(arts, gallows_state, phrase, eliminated);
+        while (1) {
+            printf("Guess a letter: ");
+            guess = read_letter();
+
+            if (!is_lowercase_letter(guess)) {
+                continue;
+            } else if (string_contains_character(eliminated, guess)
+                       || string_contains_character(phrase, guess)) {
+                continue;
+            } else {
+                break; // Valid letter entered
+            }
+        }
+        if (string_contains_character(secret, guess)) {
+            update_phrase(secret, phrase, guess);
+            if (strcmp(user_secret, phrase) == 0) {
+                print_game_state(arts, gallows_state, phrase, eliminated);
+
+                printf("You win! The secret phrase was: %s\n", secret);
+                game_over = true;
+            }
+        } else {
+            eliminated[eliminated_count++] = guess;
+            incorrect_attempts++;
+            gallows_state = incorrect_attempts;
+
+            if (incorrect_attempts == LOSING_MISTAKE) {
+                print_game_state(arts, gallows_state, phrase, eliminated);
+
+                printf("You lose! The secret phrase was: %s\n", secret);
+                game_over = true;
+            }
+        }
+    }
     return 0;
 }
+
