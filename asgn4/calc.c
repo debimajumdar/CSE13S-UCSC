b7d1a64df0bdb22d99a8b973e05b0dbaf37dd3f3
demajumd <demajumd@ucsc.edu>
2024-02-15 03:40:12 +0000

    please

--- asgn4/calc.c
+++ asgn4/calc.c
@@ -1,3 +1,6 @@
+//Filename: calc.c
+//Author name: Debi Majumdar
+
 #include "mathlib.h"
 #include "messages.h"
 #include "operators.h"
@@ -11,178 +14,191 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int opt;
-    bool my_math = true;
+    int choice;
+    bool use_advanced_math = true;
 
-    while ((opt = getopt(argc, argv, "hm")) != -1) {
-        switch (opt) {
-        case 'm': my_math = false; break;
-        case 'h': printf(USAGE, "./calc"); return 0;
-        case '?': break;
+    // Parsing command line options
+    while ((choice = getopt(argc, argv, "hm")) != -1) {
+        switch (choice) {
+        case 'm': use_advanced_math = false; break; // Disable advanced math functions
+        case 'h': printf(USAGE, "./calc"); return 0; // Display help message
+        case '?': break; // Handle unknown options
         }
     }
 
+    // Main loop for processing expressions
     while (1) {
-        char expr[1024];
-        char *saveptr;
-        bool error = false;
-        double holder;
+        char expression[1024];
+        char *token_pointer;
+        bool has_error = false;
+        double storage;
 
+        // Prompting user for input
         fprintf(stderr, "> ");
-        if (fgets(expr, sizeof(expr), stdin) != NULL) {
-            size_t len = strlen(expr);
+        if (fgets(expression, sizeof(expression), stdin) != NULL) {
+            size_t expression_length = strlen(expression);
 
-            if (len > 0 && expr[len - 1] == '\n') {
-                expr[len - 1] = '\0';
+            if (expression_length > 0 && expression[expression_length - 1] == '\n') {
+                expression[expression_length - 1] = '\0';
             }
         } else {
-            return 0;
+            return 0; // End program if no input is provided
         }
 
-        //token fgets input algorithm is from Kerry Veenstra asgn3.pdf pages 8-9
-        const char *token = strtok_r(expr, " ", &saveptr);
+        // Tokenizing input expression
+        const char *current_token = strtok_r(expression, " ", &token_pointer);
 
-        while (token != NULL && !error) {
-            int bin = 0, unary = 0;
-            int pocket = (int) strlen(token);
+        while (current_token != NULL && !has_error) {
+            int is_binary = 0, is_unary = 0;
+            int token_length = (int) strlen(current_token);
 
+            // Checking if token is a binary operator
             for (int i = 0; i < 256; i++) {
-                if (binary_operators[i] != NULL && i == *token && pocket == 1) {
-                    bin = 1;
+                if (binary_operators[i] != NULL && i == *current_token && token_length == 1) {
+                    is_binary = 1;
                     break;
                 }
             }
+            // Checking if token is a unary operator
             for (int i = 0; i < 256; i++) {
-                if (my_unary_operators[i] != NULL && i == *token && pocket == 1) {
-                    unary = 1;
+                if (my_unary_operators[i] != NULL && i == *current_token && token_length == 1) {
+                    is_unary = 1;
                     break;
                 }
             }
 
-            if (!parse_double(token, &holder) && bin == 0 && unary == 0) {
-                if (strlen(token) == 1) {
-                    fprintf(stderr, ERROR_BAD_CHAR, token[0]);
-                    error = true;
+            // Parsing numeric value or handling invalid token
+            if (!parse_double(current_token, &storage) && is_binary == 0 && is_unary == 0) {
+                if (strlen(current_token) == 1) {
+                    fprintf(stderr, ERROR_BAD_CHAR, current_token[0]);
+                    has_error = true;
                 } else {
-                    fprintf(stderr, ERROR_BAD_STRING, token);
-                    error = true;
+                    fprintf(stderr, ERROR_BAD_STRING, current_token);
+                    has_error = true;
                 }
             }
-            if (parse_double(token, &holder)) {
-                if (!stack_push(holder)) {
-                    error = true;
+            if (parse_double(current_token, &storage)) {
+                if (!stack_push(storage)) {
+                    has_error = true;
                 }
             }
 
-            if (bin == 1) {
-                switch (token[0]) {
+            // Applying binary operator if applicable
+            if (is_binary == 1) {
+                switch (current_token[0]) {
                 case '+':
                     if (!apply_binary_operator(operator_add)) {
                         fprintf(stderr, ERROR_BINARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case '-':
                     if (!apply_binary_operator(operator_sub)) {
                         fprintf(stderr, ERROR_BINARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case '*':
                     if (!apply_binary_operator(operator_mul)) {
                         fprintf(stderr, ERROR_BINARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case '/':
                     if (!apply_binary_operator(operator_div)) {
                         fprintf(stderr, ERROR_BINARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case '%':
                     if (!apply_binary_operator(fmod)) {
                         fprintf(stderr, ERROR_BINARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 }
             }
-            if (unary == 1 && my_math == true) {
-                switch (token[0]) {
+            // Applying unary operator if applicable (using advanced math functions)
+            if (is_unary == 1 && use_advanced_math == true) {
+                switch (current_token[0]) {
                 case 's':
                     if (!apply_unary_operator(Sin)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'c':
                     if (!apply_unary_operator(Cos)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 't':
                     if (!apply_unary_operator(Tan)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'a':
                     if (!apply_unary_operator(Abs)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'r':
                     if (!apply_unary_operator(Sqrt)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 }
             }
-            if (unary == 1 && my_math == false) {
-                switch (token[0]) {
+            // Applying unary operator if applicable (using standard math functions)
+            if (is_unary == 1 && use_advanced_math == false) {
+                switch (current_token[0]) {
                 case 's':
                     if (!apply_unary_operator(sin)) {
+
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'c':
                     if (!apply_unary_operator(cos)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 't':
                     if (!apply_unary_operator(tan)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'a':
                     if (!apply_unary_operator(fabs)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 case 'r':
                     if (!apply_unary_operator(sqrt)) {
                         fprintf(stderr, ERROR_UNARY_OPERATOR);
-                        error = true;
+                        has_error = true;
                     }
                     break;
                 }
             }
-            token = strtok_r(NULL, " ", &saveptr);
+            current_token = strtok_r(NULL, " ", &token_pointer);
         }
 
-        if (error == false) {
+        // Printing result or error message
+        if (!has_error) {
             stack_print();
             printf("%c", '\n');
         }
+
+        // Clearing the stack for the next expression
         stack_clear();
     }
 
